ae05fbecb309e7fa77630f206913d948199a9279
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-09 10:39:38 -0700

    completed primitive types exercise

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/03-Primitive-Types/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/03-Primitive-Types/Unsolved/script.js
@@ -1,23 +1,23 @@
-var one = 64;
-var two = "656302";
-var three = false;
-var four = 64.55;
-var five = "Howdy!";
-var six;
+var one = 64; 
+var two = "656302"; 
+var three = false; 
+var four = 64.55; 
+var five = "Howdy!"; 
+var six; 
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof one); 
-console.log(typeof two); 
-console.log(typeof three); 
-console.log(typeof four); 
-console.log(typeof five);
-console.log(typeof six);
+console.log(typeof one); //number
+console.log(typeof two); //string
+console.log(typeof three); //boolean
+console.log(typeof four); //number
+console.log(typeof five); //string
+console.log(typeof six); //undefined
 
 four = "Hello!";
 five = false;
 six = 23;
 
 // Insert comments to explain what each console log below will log to the console
-console.log(typeof four);
-console.log(typeof five);
-console.log(typeof six);
+console.log(typeof four); //string
+console.log(typeof five); //boolean
+console.log(typeof six); //number
