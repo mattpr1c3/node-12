6922b6395577e198042b77f6d1588d201093ce6b
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-09 09:34:57 -0700

    did JS intro and vars exercise

--- Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/01-JS-Intro&Vars/exercises/02-Hello-Variable/Unsolved/script.js
@@ -1,1 +1,21 @@
 // Write Your JavaScript Code Here
+var personName = "Matthew"
+
+console.log(`My name is ${personName}`)
+
+var pets = 2
+
+console.log("I have " + pets + " pets.")
+
+var funFact = "I like basketball."
+
+console.log("Fun fact: " + funFact)
+
+personName = "Ross"
+console.log(`My name is ${personName}`)
+
+pets = 2
+console.log("I have " + pets + " pets.")
+
+funFact = "I go kayak fishing out in the ocean."
+console.log("Fun fact: " + funFact)
