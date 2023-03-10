32bb0d2096ac92057ecc5242fe26f261338eaa2a
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-10 12:54:13 -0700

    did function arithmetic exercise

--- zeAlgos/01-arithmetic/Unsolved/arithmetic.js
+++ zeAlgos/01-arithmetic/Unsolved/arithmetic.js
@@ -1,9 +1,22 @@
 // Fill in the body of each function with the code required
 
-var add = function(num1, num2) {};
+var add = function(num1, num2) {
+    var result = num1 + num2;
+    return result
 
-var subtract = function(num1, num2) {};
+};
 
-var multiply = function(num1, num2) {};
+var subtract = function(num1, num2) {
+    var result = num1 - num2;
+    return result
+};
 
-var divide = function(num1, num2) {};
+var multiply = function(num1, num2) {
+    var result = num1 * num2;
+    return result
+};
+
+var divide = function(num1, num2) {
+    var result = num1 / num2;
+    return result
+};
