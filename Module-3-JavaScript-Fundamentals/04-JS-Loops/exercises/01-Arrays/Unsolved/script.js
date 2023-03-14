7b53c3e23221f7b5155faaee3a77bde10e27bc43
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-14 10:09:01 -0700

    did arrays exercise

--- Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
+++ Module-3-JavaScript-Fundamentals/04-JS-Loops/exercises/01-Arrays/Unsolved/script.js
@@ -1,1 +1,15 @@
 // WRITE YOUR CODE HERE
+let student_list = ["Andre", "Karl", "Rashida", "Olivia"]
+
+console.log(student_list.length);
+
+console.log("Welcome to the class " + student_list[0]);
+console.log("Welcome to the class " + student_list[1]);
+console.log("Welcome to the class " + student_list[2]);
+console.log("Welcome to the class " + student_list[3]);
+
+student_list[0] = "Jeff";
+
+if (student_list[0] === "Jeff") {
+    console.log("Jeff is in class")
+}
