65313ec7e31d5db74cbb988485643d2c0de026d1
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-03-24 12:14:19 -0700

    did some practice/exercises

--- Module-04-Advanced-JS/07-JS-Promises/exercises/01-Promise-All/Unsolved/index.js
+++ Module-04-Advanced-JS/07-JS-Promises/exercises/01-Promise-All/Unsolved/index.js
@@ -5,12 +5,22 @@ const callAPI = (duration) =>
   new Promise((resolve, reject) => {
     setTimeout(() => {
       // TODO: If the duration is longer than maxDuration, reject() the promise
-      // TODO: Otherwise resolve() the promise
+      if (duration > maxDuration) {
+        reject(new Error("Something went wrong"));
+        // TODO: Otherwise resolve() the promise
+      } else {
+        resolve("Resolved");
+      }
     }, duration);
   });
 
 const promises = apiCallDurations.map((duration) => callAPI(duration));
 
-console.log('Promises array before the timeouts have finished: ', promises);
+console.log("Promises array before the timeouts have finished: ", promises);
 
 // TODO: Use Promise.all() to capture when the array of promises has been resolved or if any of them were rejected
+Promise.all(promises)
+  .then((values) => {
+    console.log(values);
+  })
+  .catch((err) => new Error(err));
