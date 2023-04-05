2ced29852042a678e484bbfed921306f96e233ce
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-04-05 12:48:11 -0700

    did practice

--- zeAlgos/02-odd-or-even/Unsolved/odd-or-even.js
+++ zeAlgos/02-odd-or-even/Unsolved/odd-or-even.js
@@ -1,4 +1,14 @@
 // Write code to return the string "odd" if `num` is an odd number
 // and return the string "even" if `num` is an even number
 
-var oddOrEven = function(num) {};
+var oddOrEven = function (num) {
+  if (num % 2 == 0) {
+    console.log("even");
+    return "even"
+  } else {
+    console.log("odd");
+    return "odd"
+  }
+};
+
+oddOrEven(100304);
