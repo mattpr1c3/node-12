1ee7652c43e2be4b9ef9710e00c5191f86e4adf9
mattpr1c3 <matthew.pr1c3@gmail.com>
2023-04-10 07:03:29 -0700

    practice

--- zeAlgos/03-is-palindrome/Unsolved/is-palindrome.js
+++ zeAlgos/03-is-palindrome/Unsolved/is-palindrome.js
@@ -1,3 +1,6 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
-var isPalindrome = function(str) {};
+var isPalindrome = function(str) {
+    let result = str.split("").reverse().join("")
+    return (str == result)
+};
--- zeAlgos/04-vowel-count/Unsolved/vowel-count.js
+++ zeAlgos/04-vowel-count/Unsolved/vowel-count.js
@@ -1,3 +1,13 @@
 // Write code to return the the number of vowels in `str`
 
-var vowelCount = function(str) {};
+var vowelCount = function (str) {
+  let numVowels = 0;
+  const vowels = ["a", "e", "i", "o", "u",];
+  let str1 = str.toLowerCase()
+  for (let char of str1) {
+    if (vowels.includes(char)) {
+      numVowels++;
+    }
+  }
+  return numVowels;
+};
